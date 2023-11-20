48e9b79cd7b2760372eb47ac2c1de4b415f70176
Henry Baldursson <henry.baldursson@gmail.com>
2023-11-20 14:58:52 +0000

    Update live-greps def

--- nvim/lua/plugins/live_grep_args.lua
+++ nvim/lua/plugins/live_grep_args.lua
@@ -8,7 +8,7 @@ return {
           {
             "<leader>fW",
             ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-            "Live grep (Args)",
+            desc = "Live grep (Args)",
           },
         },
       },
@@ -17,7 +17,8 @@ return {
       require("telescope").load_extension("live_grep_args")
     end,
     keys = {
-      { "<leader>fg", "<cmd> Telescope git_files <CR>", "Find in git" },
+      { "<leader>fg", "<cmd> Telescope git_files <CR>", desc = "Find in git" },
+      { "<leader>fw", "<cmd> Telescope live_grep <CR>", desc = "Live grep" },
     },
   },
 }
