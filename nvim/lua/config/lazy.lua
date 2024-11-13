3f2b2951dfd64ad1d5f63d9d78ada05cc475b37c
Henry Thor Baldursson <henry@sidekickhealth.com>
2024-11-13 13:44:03 +0000

    Updating LazyVim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,8 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.python" },
